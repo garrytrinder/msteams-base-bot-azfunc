429ff598c6026085a1aec5f05f4143eb41dd83a6
Garry Trinder <garry@trinder365.co.uk>
2023-02-17 20:08:50 +0000

    Update lifecycle methods

--- bot/src/internal/activityHandler.ts
+++ bot/src/internal/activityHandler.ts
@@ -14,26 +14,14 @@ export class BotActivityHandler extends TeamsActivityHandler {
         this.onMessage(async (context, next) => {
             if (context.activity.text.startsWith('/clear')) {
                 await nameStateAccessor.delete(context);
-                await context.sendActivity('State cleared');
+                await context.sendActivity('Clean 🧼✨');
             }
             await next();
         });
 
         // handle app install event
         this.onInstallationUpdateAdd(async (context, next) => {
-            await context.sendActivity(`You installed the app`);
-            await next();
-        });
-
-        // handle app uninstall event
-        this.onInstallationUpdateRemove(async (context, next) => {
-            await context.sendActivity(`You removed the app`);
-            await next();
-        });
-
-        // handle app update event
-        this.onInstallationUpdate(async (context, next) => {
-            await context.sendActivity(`You updated the app`);
+            await context.sendActivity(`Hi 👋`);
             await next();
         });
     }
